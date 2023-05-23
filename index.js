72a83bb0653963c700cd09f1ee566e6efff52bfb
Sandeep Gill <sandeepgill62@gmail.com>
2023-05-23 22:21:58 +0000

    API Call #2: Fetch Geo Coordinates By IP

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // index.js
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
+//call fucntion  to fecth IP address
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!", error);
@@ -8,4 +9,14 @@ fetchMyIP((error, ip) => {
   }
 
   console.log('It worked! Returned IP:', ip);
+});
+
+//call function to fetch coordinates
+fetchCoordsByIP('24.77.87.224', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:', coordinates);
 });
--- iss.js
+++ iss.js
@@ -20,10 +20,45 @@ const fetchMyIP = function (callback) {
       return;
     }
 
+    //get IP
     const ip = JSON.parse(body).ip;
     callback(null, ip);
   });
 };
 
-module.exports = { fetchMyIP };
+/**
+ * Makes a single API request to retrieve the lat/lng for a given IPv4 address.
+ * Input:
+ *   - The ip (ipv4) address (string)
+ *   - A callback (to pass back an error or the lat/lng object)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The lat and lng as an object (null if error). Example:
+ *     { latitude: '49.27670', longitude: '-123.13000' }
+ */
+const fetchCoordsByIP = function (ip, callback) {
+  request(`http://ipwho.is/${ip}`, (error, response, body) => {
+
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    const parsedBody = JSON.parse(body);
+
+    if (!parsedBody.success) {
+      const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
+      callback(Error(message), null);
+      return;
+    }
+
+    //get co-ordinates
+    const { latitude, longitude } = parsedBody;
+
+    callback(null, { latitude, longitude });
+  });
+};
+
+//export module
+module.exports = { fetchMyIP, fetchCoordsByIP };
 
