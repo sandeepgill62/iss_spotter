73b53f417c2db603b349e8bf7ac19ede72eb3982
Sandeep Gill <sandeepgill62@gmail.com>
2023-05-24 08:07:00 +0000

    implementing the iss code using promises

---
+++ index2.js
@@ -0,0 +1,22 @@
+// index2.js
+
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+const printPassTimes = function (passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+// Call 
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
+
---
+++ iss_promised.js
@@ -0,0 +1,49 @@
+// iss_promised.js
+const request = require('request-promise-native');
+
+/*
+ * Requests user's ip address from https://www.ipify.org/
+ * Input: None
+ * Returns: Promise of request for ip data, returned as JSON string
+ */
+const fetchMyIP = function () {
+  return request('https://api.ipify.org?format=json');
+};
+
+/*
+ * Requests coordinates of ip address from https://www.ipwho.is/
+ * Input: JSON body containing ip data response from ipify.org
+ * Returns: Promise of request for coordidata, returned as JSON string
+ */
+
+const fetchCoordsByIP = function (body) {
+  const ip = JSON.parse(body).ip;
+  return request(`http://ipwho.is/${ip}`);
+};
+
+/*
+ * Requests data from https://iss-flyover.herokuapp.com using provided lat/long data
+ * Input: JSON body containing geo data response from ipwho.is
+ * Returns: Promise of request for fly over data, returned as JSON string
+ */
+const fetchISSFlyOverTimes = function (body) {
+  const { latitude, longitude } = JSON.parse(body);
+  const url = `https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`;
+  return request(url);
+};
+
+/* 
+ * Input: None
+ * Returns: Promise for fly over data for users location
+ */
+const nextISSTimesForMyLocation = function () {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation };
